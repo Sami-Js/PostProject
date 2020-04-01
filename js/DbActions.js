d32849f69165c6e6353010a600083c3dedea2976
Bamoooh <45263225+Bamoooh@users.noreply.github.com>
2020-04-01 04:35:44 +0300

    text fixes and added dbactions.js
    fixed naming of questions, added question texts that were missing, fixed capitalization, changed yes/no to Yes/No in 'data-value' attributes, added attribute 'value' with 'true/false' values seperated dredging form from other form (needs a check).

---
+++ js/DbActions.js
@@ -0,0 +1,131 @@
+// MUST TRY TO GET ESQID FROM URL IN CASE OF UPDATED INFO
+var esqID = ""
+var url = 'http://localhost:5001/api';
+//PATHS
+var paths = {
+    "rawM": "/rawM",
+    "aux": "/aux",
+    "Products": "/Products",
+    "HazardousMaterials": "/HazardousMaterials",
+    "Fuels": "/Fuels",
+    "AirEmissionsPointSources": "/AirEmissionsPointSources",
+    "FugitiveAirEmissions": "/FugitiveAirEmissions",
+    "WaterConsumption": "/WaterConsumption",
+    "EffluentWastewater": "/EffluentWastewater",
+    "SolidLiquidWaste": "/SolidLiquidWaste",
+    "Noise": "/Noise",
+    "Flare": "/Flare",
+    "Incinerator": "/Incinerator",
+    "LoadUnload": "/LoadUnload",
+    "DredgingOperations": "/DredgingOperations",
+    "OtherInformation": "/OtherInformation",
+}
+onClickSubmit()
+//Get next btn
+// btn = document.getElementById("Next")
+// btn.addEventListener('click', function () {
+//     onClickSubmit()
+// })
+function onClickSubmit() {
+    try {
+        data = new FormData(document.getElementById("Env"))
+        var jsonBody = setupObject(data)
+        if (esqID === "")
+            esqID = postData(url + "/EnvForm", setupObject(data));
+        else
+            updateData(url + "/EnvForm", data)
+
+        for (var key in paths) {
+            try {
+                path = url + paths[key]
+                data = new FormData(document.getElementById(key + "Form"))
+                jsonData = setupArrayData(data, key)
+                // console.log(jsonData)
+                // postData(path, data, 1)
+                jsonBody[key] = jsonData
+            } catch{ }
+        }
+        console.log(jsonBody)
+    }
+    catch{
+        console.log(`failed submit ${path} where`)
+        // console.log(`Form = ${key}Form`)
+        // console.log(`Controller = ${paths[key]}`)
+    }
+}
+
+function setupObject(data) {
+    var json = new Object();
+    for (var [key, val] of data.entries()) {
+        if (val === 'true')
+            val = true
+        else if (val === 'false')
+            val = false
+        json[key] = val
+    }
+    return json
+}
+//JSON OBJECT SETUP
+function setupArrayData(data, objName) {
+    var tempArr = []
+    var temp = new Object()
+    for (var [key, value] of data.entries()) {
+        if (key == "empty") {
+            tempArr.push(temp)
+            temp = new Object()
+        }else{
+            if (value == "true") {
+                value = true
+            } else if (value == "false") {
+                value = false
+            }
+            temp[key] = value
+        }
+    }
+    return tempArr
+}
+
+//SEND REQUEST BLOCK
+function postData(path, body) {
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", path, 1)
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300)
+            console.log("success")
+        else
+            console.log("fail")
+        // response = xhr.responseText
+        // console.log(this.responseText)
+
+    }
+    xhr.send(JSON.stringify(body))
+    // console.log(xhr.responseText)
+    return xhr.responseText
+}
+
+//SUBMIT ARRAY OF OBJECTS TEST
+// document.getElementById("submit").addEventListener('click', function(){
+
+
+
+// var paths = {
+//     env : "/EnvForm",
+//     rawMaterials : "/rawM",
+//     aux : "/aux",
+//     products : "/Products",
+//     hazards : "/HazardousMaterials",
+//     fuels : "/Fuels",
+//     air : "/AirEmissionsPointSources",
+//     fugAir : "/FugitiveAirEmissions",
+//     water : "/WaterConsumption",
+//     effluent : "/EffluentWastewater",
+//     solid : "/SolidLiquidWaste",
+//     noise : "/Noise",
+//     flare : "/Flare",
+//     incinerator : "/Incinerator",
+//     load : "/LoadUnload",
+//     dredge : "/DredgingOperations",
+//     other : "/OtherInformation", 
+// }
